5c9d7e4bf91954f6502b263ede9a019d0f8832cb
Bantonio12 <thcha0727@gmail.com>
2022-05-11 17:16:36 -0400

    hi 4

--- start_page/urls.py
+++ start_page/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.start_page_views, name='start_page_views'),
 ]
--- start_page/views.py
+++ start_page/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 
-def index(request):
+def start_page_views(request):
     return HttpResponse("start page fuck uoft")
