6820b39d510a2ad37368acc783c92caf297acf13
moon990519 <moon990519@gmail.com>
2022-05-11 20:45:41 -0300

    html added

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'start_page'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\Users\moon9\uoft_student_trade\mysite\start_page\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
