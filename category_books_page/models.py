b3a5b92ca5b8f141648f4b5a4e3e70e1a3111198
moon990519 <moon990519@gmail.com>
2022-05-13 18:16:33 -0300

    model of the book category is set

--- category_books_page/models.py
+++ category_books_page/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class BookProducts(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    price = models.IntegerField()
+    summary = models.TextField(default='The summary is shown')
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'start_page'
+    'category_books_page',
 ]
 
 MIDDLEWARE = [
