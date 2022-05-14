036610c325d479107d34a520de8f35126631dee7
moon990519 <moon990519@gmail.com>
2022-05-13 23:00:32 -0300

    book database changed

--- category_books_page/models.py
+++ category_books_page/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class BookProducts(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.IntegerField()
+    title = models.CharField(max_length=60)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=5,decimal_places=2)
     summary = models.TextField(default='The summary is shown')
+
